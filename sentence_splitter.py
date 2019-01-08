62b170eb8d73ae8c2c67c018e9fe13f757829dd1
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-08 17:57:42 +0100

    Get slice of text from index for check

--- sentence_splitter.py
+++ sentence_splitter.py
@@ -9,20 +9,17 @@ def get_position_to_check(text_from_file, index_list):
     try:
         question_index = text_from_file.index('?')
     except ValueError:
-        print("There are no ? in given text.")
         question_index = None
 
     try:
         exclamation_index = text_from_file.index('!')
     except ValueError:
-        print("There are no ! in given text.")
         exclamation_index = None
 
     dot_index = text_from_file.index('.')
     if dot_index in index_list:
         dot_index = text_from_file[index_list[-1] + 1:].index('.') + index_list[-1]
 
-    print("? {0}, ! {1}, . {2}".format(question_index, exclamation_index, dot_index))
     index = dot_index
     if question_index is not None:
         if question_index < index:
@@ -30,7 +27,7 @@ def get_position_to_check(text_from_file, index_list):
     if exclamation_index is not None:
         if exclamation_index < index:
             index = exclamation_index
-    print("index_list: {0}".format(index_list))             # TODO: delete
+
     return index
 
 
@@ -40,9 +37,12 @@ def get_char_at_index(text_from_file, index):
 
 
 def get_sample_slice(text_from_file, index):
-    # TODO: ma wyciagnac z tekstu wycinek z '.' 2 znaki za '.' i 3 znaki przed '.'
-    # return slice_sample
-    pass
+    slice_begining = index - 3
+    if slice_begining < 0:
+        slice_begining = index - 2
+    sample_slice = text_from_file[slice_begining:index + 3]
+
+    return sample_slice
 
 
 def check_slice_with_dot(sample_slice):
@@ -62,26 +62,26 @@ def main():
     index_list = []
     text_from_file = get_file_content()
     position_to_check = get_position_to_check(text_from_file, index_list)
-    print(position_to_check)                                                            # TODO: delete
+    # print("position to check: {0}".format(position_to_check))                                    # TODO: delete
     char_at_index = get_char_at_index(text_from_file, position_to_check)
-    print(char_at_index)                                                                # TODO: delete
-    # if char_at_index == '?':
-    #     extract_sentence(text_from_file, position_to_check, sentence_list)
-    #     sentence_list = []
-    #     text_from_file = text_from_file[position_to_check + 1:]
-    # elif char_at_index == '!':
-    #     extract_sentence(text_from_file, position_to_check, sentence_list)
-    #     sentence_list = []
-    #     text_from_file = text_from_file[position_to_check + 1:]
-    # elif char_at_index == '.':
-    #     sample_slice = get_sample_slice(text_from_file, position_to_check)
-    #     check_result = check_slice_with_dot(sample_slice)
-    #     if check_result is True:
-    #         extract_sentence(text_from_file, position_to_check, sentence_list)
-    #         sentence_list = []
-    #         text_from_file = text_from_file[position_to_check + 1:]
-    #     else:
-    #         index_list.append(position_to_check)
+    # print("char_at_index: {0}".format(char_at_index))                                            # TODO: delete
+    if char_at_index == '?':
+        extract_sentence(text_from_file, position_to_check, sentence_list)
+        sentence_list = []
+        text_from_file = text_from_file[position_to_check + 1:]
+    elif char_at_index == '!':
+        extract_sentence(text_from_file, position_to_check, sentence_list)
+        sentence_list = []
+        text_from_file = text_from_file[position_to_check + 1:]
+    elif char_at_index == '.':
+        sample_slice = get_sample_slice(text_from_file, position_to_check)
+        check_result = check_slice_with_dot(sample_slice)
+        if check_result is True:
+            extract_sentence(text_from_file, position_to_check, sentence_list)
+            index_list = []
+            text_from_file = text_from_file[position_to_check + 1:]
+        else:
+            index_list.append(position_to_check)
 
 
 if __name__ == '__main__':
